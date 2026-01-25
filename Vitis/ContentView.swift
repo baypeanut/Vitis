5808cc42e6f386764336a4794d462743293aff56
Noah Dericioglu <aderici@unc.edu>
2026-01-25 17:43:17 -0500

    Initial Commit

---
+++ Vitis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vitis
+//
+//  Created by Noah Ahmet Dericioglu on 1/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vitis/VitisApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitisApp.swift
+//  Vitis
+//
+//  Created by Noah Ahmet Dericioglu on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct VitisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
